f131b61d778c8e77f393d0c4f29f6261cb25b34e
Oliver <1571880470@qq.com>
2023-02-22 11:49:53 +0800

    add action function for every picker

--- lua/telescope/_extensions/find_pickers/main.lua
+++ lua/telescope/_extensions/find_pickers/main.lua
@@ -27,12 +27,13 @@ M.find_pickers = function(opts)
 
   local excluded = extensions_pickers._config.find_pickers.excluded or {}
   local plugin_opts = extensions_pickers._config.find_pickers.opts or {}
-  print(vim.inspect(plugin_opts))
+  local funcs = extensions_pickers._config.find_pickers.actions or {}
+  -- print(vim.inspect(funcs))
 
   for name, item in pairs(builtin_pickers) do
     if not (vim.tbl_contains(excluded, name)) then
       result_table[name] = {
-        action = item or function() end,
+        action = funcs[name] or item or function() end,
         opt = plugin_opts[name] or opts_pickers,
       }
     end
@@ -41,7 +42,7 @@ M.find_pickers = function(opts)
   for name, item in pairs(extensions_pickers.manager) do
     if not (vim.tbl_contains(excluded, name)) then
       result_table[name] = {
-        action = item[name] or function() end,
+        action = funcs[name] or item[name] or function() end,
         opt = plugin_opts[name] or opts_pickers,
       }
     end
