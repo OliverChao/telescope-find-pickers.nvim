72eeb5d7ebe1d2af37e4c2a219a6643e1e9d9829
Oliver <1571880470@qq.com>
2023-02-22 11:18:42 +0800

    format stylua

--- lua/telescope/_extensions/find_pickers.lua
+++ lua/telescope/_extensions/find_pickers.lua
@@ -2,10 +2,10 @@ local has_telescope, telescope = pcall(require, "telescope")
 local main = require("telescope._extensions.find_pickers.main")
 
 if not has_telescope then
-	error("This plugins requires nvim-telescope/telescope.nvim")
+  error("This plugins requires nvim-telescope/telescope.nvim")
 end
 
 return telescope.register_extension({
-	setup = main.setup,
-	exports = { find_pickers = main.find_pickers },
+  setup = main.setup,
+  exports = { find_pickers = main.find_pickers },
 })
--- lua/telescope/_extensions/find_pickers/main.lua
+++ lua/telescope/_extensions/find_pickers/main.lua
@@ -16,56 +16,57 @@ M.setup = function(setup_config) end
 
 -- This creates a picker with a list of all of the pickers
 M.find_pickers = function(opts)
-    local opts_find_pickers = opts or themes.get_dropdown(opts)
-    local opts_pickers = {
-        bufnr = vim.api.nvim_get_current_buf(),
-        winnr = vim.api.nvim_get_current_win(),
-    }
+  local opts_find_pickers = opts or themes.get_dropdown(opts)
+  local opts_pickers = {
+    bufnr = vim.api.nvim_get_current_buf(),
+    winnr = vim.api.nvim_get_current_win(),
+  }
 
-    -- Variables that setup can change
-    local result_table = {}
+  -- Variables that setup can change
+  local result_table = {}
 
-    local excluded = extensions_pickers._config.find_pickers.excluded or {}
-    local plugin_opts = extensions_pickers._config.find_pickers.opts or {}
-    print(vim.inspect(plugin_opts))
+  local excluded = extensions_pickers._config.find_pickers.excluded or {}
+  local plugin_opts = extensions_pickers._config.find_pickers.opts or {}
+  print(vim.inspect(plugin_opts))
 
-
-    for name, item in pairs(builtin_pickers) do
-        if not (vim.tbl_contains(excluded, name)) then
-            result_table[name] = {
-                action = item or function() end,
-                opt = plugin_opts[name] or opts_pickers,
-            }
-        end
+  for name, item in pairs(builtin_pickers) do
+    if not (vim.tbl_contains(excluded, name)) then
+      result_table[name] = {
+        action = item or function() end,
+        opt = plugin_opts[name] or opts_pickers,
+      }
     end
+  end
 
-    for name, item in pairs(extensions_pickers.manager) do
-        if not (vim.tbl_contains(excluded, name)) then
-            result_table[name] = {
-                action = item[name] or function() end,
-                opt = plugin_opts[name] or opts_pickers,
-            }
-        end
+  for name, item in pairs(extensions_pickers.manager) do
+    if not (vim.tbl_contains(excluded, name)) then
+      result_table[name] = {
+        action = item[name] or function() end,
+        opt = plugin_opts[name] or opts_pickers,
+      }
     end
+  end
 
-    pickers.new(opts_find_pickers or {}, {
-        prompt_title = "Find Pickers",
-        results_title = "Picker",
-        finder = finders.new_table({
-            results = vim.tbl_keys(result_table),
-        }),
-        attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-                local selection = actions_state.get_selected_entry()
-                local value = selection.value
+  pickers
+    .new(opts_find_pickers or {}, {
+      prompt_title = "Find Pickers",
+      results_title = "Picker",
+      finder = finders.new_table({
+        results = vim.tbl_keys(result_table),
+      }),
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          local selection = actions_state.get_selected_entry()
+          local value = selection.value
 
-                actions.close(prompt_bufnr)
-                result_table[value].action(result_table[value].opt)
-            end)
-            return true
-        end,
-        sorter = conf.file_sorter(opts_find_pickers),
-    }):find()
+          actions.close(prompt_bufnr)
+          result_table[value].action(result_table[value].opt)
+        end)
+        return true
+      end,
+      sorter = conf.file_sorter(opts_find_pickers),
+    })
+    :find()
 end
 
 return M
