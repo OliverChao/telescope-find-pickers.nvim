77ba402db729d84a4051921a4886c00fc08f79ec
Oliver <fei.chao.1009@gmail.com>
2023-03-07 20:06:00 +0800

    clean comment

--- lua/telescope-find-pickers/init.lua
+++ lua/telescope-find-pickers/init.lua
@@ -2,19 +2,15 @@ local builtin_pickers = require("telescope.builtin")
 local extensions_pickers = require("telescope._extensions")
 
 local result_table = {}
--- vim.notify("hahahhah")
--- print("hahahahahhahaha")
+
 local opts_pickers = {
   bufnr = vim.api.nvim_get_current_buf(),
   winnr = vim.api.nvim_get_current_win(),
 }
 
--- Variables that setup can change
-
 local excluded = extensions_pickers._config.find_pickers.excluded or {}
 local plugin_opts = extensions_pickers._config.find_pickers.opts or {}
 local funcs = extensions_pickers._config.find_pickers.actions or {}
--- print(vim.inspect(funcs))
 
 for name, item in pairs(builtin_pickers) do
   if not (vim.tbl_contains(excluded, name)) then
